849b8802e65e3c8707efb4028fb1d0f4ef4bed5a
ShankarjTechnocredits <shankardjadhav12@gmail.com>
2019-05-05 13:04:09 +0530

    adding swap numbers program

---
+++ src/shankarJ/SwapNumbers.java
@@ -0,0 +1,21 @@
+package assignments;
+
+//without using third variable.
+public class SwapNumbers {
+
+	public static void main(String[] args) {
+		SwapNumbers sn = new SwapNumbers();
+		int x, y;
+		x = 10;
+		y = 5;
+		System.out.println(sn);
+		System.out.println("Before swapping - x: " + x + ", y: " + y);
+
+		x = x + y;
+		y = x - y;
+		x = x - y;
+		System.out.println("After swapping  - x: " + x + ", y: " + y);
+
+	}
+
+}
