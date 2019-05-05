d9c78e87cce04f5824e719466603db7ab06b56ed
ShankarjTechnocredits <shankardjadhav12@gmail.com>
2019-05-05 13:57:15 +0530

    adding swap numbers using third variable

---
+++ src/shankarJ/SwapNumbers3rd.java
@@ -0,0 +1,27 @@
+package assignments;
+//using third variable
+public class SwapNumbers3rd {
+
+	public static void main(String[] args) {
+
+		SwapNumbers3rd sn = new SwapNumbers3rd();
+		float first, second;
+		first = 67.5f;
+		second = 37.9f;
+		System.out.println(sn);
+
+		System.out.println("--Before swap--");
+		System.out.println("First number = " + first);
+		System.out.println("Second number = " + second);
+
+		float third;
+		third = first;
+		first = second;
+		second = third;
+
+		System.out.println("--After swap--");
+		System.out.println("First number = " + first);
+		System.out.println("Second number = " + second);
+	}
+
+}
