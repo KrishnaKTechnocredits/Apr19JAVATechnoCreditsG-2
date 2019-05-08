47e8fb7be1652f74b40bdd51421b1d16cfa1827e
Navjeet Kumar <navzeet@gmail.com>
2019-05-08 07:17:46 +0530

    Printing Pattern - Example 2

--- src/navjeetK/PatternPrinting1.java
+++ src/navjeetK/PatternPrinting1.java
@@ -4,32 +4,27 @@ public class PatternPrinting1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		for(int i=1;i<=4;i++)
-		{
-			
-			if(i%2==0)
-			{
-				for(int j=1;j<=i;j++)
-				{
+
+		for (int i = 1; i <= 4; i++) {
+
+			if (i % 2 == 0) {
+				for (int j = 1; j <= i; j++) {
 					System.out.print(" # " + " ");
 				}
-				
-			
+
 			}
-			
-			else
-			{
-				for(int j=1;j<=i;j++)
-				{
+
+			else {
+				for (int j = 1; j <= i; j++) {
 					System.out.print(" * " + " ");
 				}
 			}
-			
+
 			System.out.println(" ");
-			
+
 		}
 
 	}
 
 }
+
