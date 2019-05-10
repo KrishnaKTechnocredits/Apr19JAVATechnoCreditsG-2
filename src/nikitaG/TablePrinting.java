52175637d59c705d776f93baf85da433f6b3ee56
Nikita Garad <nikib2008@gmail.com>
2019-05-10 21:29:58 +0530

    Added Assignmnet 2

---
+++ src/nikitaG/TablePrinting.java
@@ -0,0 +1,19 @@
+package nikitaG;
+
+public class TablePrinting {
+	static void display(int num) {
+		int count=1;
+		for (int i = 1; i <= num; i++) {
+			for (int j = 1; j <= num; j++) {
+				System.out.print(count*num + " ");
+				count++;
+			}
+			System.out.println();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		display(3);
+	}
+}
